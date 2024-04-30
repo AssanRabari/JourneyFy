2fcc6cc08711174500d797fbd440c594a329e30b
AssanRabari <assandesai@gmail.com>
2024-04-30 09:38:15 +0530

    searchBar design and mobile responsive

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import "./SearchBar.scss";
+
+const type = ["buy", "rent"];
 
 const SearchBar = () => {
+  const [query, setQuery] = useState({
+    type: "buy",
+    location: "",
+    minPrice: 0,
+    maxPrice: 0,
+  });
+
+  const switchType = (value) => {
+    setQuery((prev) => ({ ...prev, type: value }));
+  };
+
   return (
     <div className="searchBar">
       <div className="type">
-        <button>Buy</button>
-        <button>Rent</button>
+        {type.map((type) => (
+          <button
+            key={type}
+            onClick={() => switchType(type)}
+            className={query.type === type ? "active" : ""}
+          >
+            {type}
+          </button>
+        ))}
       </div>
       <form>
         <input type="text" name="location" placeholder="City Location" />
