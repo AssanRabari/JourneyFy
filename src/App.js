1d6df336a3939c7732da6fa5e579af811a2e714c
AssanRabari <assandesai@gmail.com>
2024-05-01 09:38:20 +0530

    setup react router dom for different pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import Navbar from "./components/navbar/Navbar";
-import "./layout.scss";
 import HomePage from "./pages/homePage/homePage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ListPage from "./pages/listPage/ListPage";
+import Layout from "./pages/layout/Layout";
+import SinglePage from "./pages/singlePage/SinglePage";
+import Login from "./pages/login/Login";
+
 function App() {
-  return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="content">
-        <HomePage />
-      </div>
-    </div>
-  );
+  const routers = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/list",
+          element: <ListPage />,
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />,
+        },
+        {
+          path: "/login",
+          element: <Login />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={routers} />;
 }
 
 export default App;
