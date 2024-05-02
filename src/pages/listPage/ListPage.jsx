68b097c4bae0ccefee25c918967f4539ae3d31c3
AssanRabari <assandesai@gmail.com>
2024-05-02 09:33:30 +0530

    hotel list page initial  design and created new components

--- src/pages/listPage/ListPage.jsx
+++ src/pages/listPage/ListPage.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import "./ListPage.scss";
-
+import { listData } from "../../lib/dummyData";
+import Filter from "../../components/filter/Filter";
+import Card from "../../components/card/Card";
 const ListPage = () => {
-  return <div>ListPage</div>;
+  const data = listData;
+  return (
+    <div className="listPage">
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter />
+          {data.map((item) => {
+            <Card />;
+          })}
+        </div>
+      </div>
+      <div className="mapContainer">Map</div>
+    </div>
+  );
 };
 
 export default ListPage;
