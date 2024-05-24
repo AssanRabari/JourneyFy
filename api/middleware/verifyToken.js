774e6f37038bb9908fd65d35164cd4801a258605
AssanRabari <assandesai@gmail.com>
2024-05-24 09:34:53 +0530

    added middleware for verify token

--- api/middleware/verifyToken.js
+++ api/middleware/verifyToken.js
@@ -1,22 +1,15 @@
-import jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken"
 
-export const shouldBeLoggedIn = async (req, res) => {
-
-
-    res.status(200).json({ message: "You are authenticated" })
-}
-
-export const shouldBeAdmin = async (req, res) => {
+export const verifyToken = (req, res, next) => {
     const token = req.cookies.token;
 
     if (!token) return res.status(401).json({ message: "Not Authenticated" })
 
     jwt.verify(token, "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTcxNjI2MzU0NCwiaWF0IjoxNzE2MjYzNTQ0fQ.NPdgz8ETPhM62UPaWs5RoTib4Hd77Yh_D4hK41EeR_g", async (err, payload) => {
         if (err) return res.status(403).json({ message: "Token is not valid" })
-        if (!payload.isAdmin) {
-            return res.status(403).json({ message: "Not Authorized" })
-        }
+        req.userId = payload.id
+
+        next()
     })
 
-    res.status(200).json({ message: "You are authenticated" })
 }
