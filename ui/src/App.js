c698b4cdb315538e4170f2d24316de5b0b8ff6fc
AssanRabari <assandesai@gmail.com>
2024-05-27 08:50:36 +0530

    protected routes with router dom

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,7 +1,7 @@
 import HomePage from "./pages/homePage/homePage";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ListPage from "./pages/listPage/ListPage";
-import Layout from "./pages/layout/Layout";
+import { Layout, RequireAuth } from "./pages/layout/Layout";
 import SinglePage from "./pages/singlePage/SinglePage";
 import Login from "./pages/login/Login";
 import ProfilePage from "./pages/profilePage/ProfilePage";
@@ -26,16 +26,22 @@ function App() {
           element: <SinglePage />,
         },
         {
-          path: "/profile",
-          element: <ProfilePage />,
-        },
-        {
           path: "/login",
           element: <Login />,
         },
         {
           path: "/register",
           element: <Register />,
+        },
+      ],
+    },
+    {
+      path: "/",
+      element: <RequireAuth />,
+      children: [
+        {
+          path: "/profile",
+          element: <ProfilePage />,
         },
       ],
     },
