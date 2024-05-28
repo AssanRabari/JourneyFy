86b95e41c922e18356e56a4e66a22e6ca5711d50
AssanRabari <assandesai@gmail.com>
2024-05-28 08:31:26 +0530

    used mongodb find and findById for users

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,17 +1,20 @@
-export const getUsers = (req, res) => {
-    console.log("getUsers")
-    try {
+import User from "../models/user.model.js"
 
+export const getUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users)
     } catch (error) {
         console.log(error)
         res.status(500).json({ message: "Failed to get users!" })
     }
 }
 
-export const getUser = (req, res) => {
-    console.log("getUser")
+export const getUser = async (req, res) => {
+    const userId = req.params.id;
     try {
-
+        const singleUser = await User.findById({ _id: userId });
+        res.status(200).json(singleUser)
     } catch (error) {
         console.log(error)
         res.status(500).json({ message: "Failed to get user!" })
