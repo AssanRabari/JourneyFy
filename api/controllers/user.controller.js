c64b2f007e56b5f0101e044039958fd80bd29351
AssanRabari <assandesai@gmail.com>
2024-05-29 09:13:48 +0530

    user crud operations api side completed

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js"
+import bycrpt from 'bcrypt';
 
 export const getUsers = async (req, res) => {
     try {
@@ -21,20 +22,36 @@ export const getUser = async (req, res) => {
     }
 }
 
-export const updateUser = (req, res) => {
-    console.log("updateUser")
-    try {
+export const updateUser = async (req, res) => {
+    const userId = req.params.id;
+    const tokenUserId = req.userId
+    const { password, ...data } = req.body;
 
+    if (userId !== tokenUserId) {
+        res.status(403).json({ message: "Not Authorized" })
+    }
+
+    let updatedPassword = null;
+    try {
+        if (password) {
+            updatedPassword = await bycrpt.hash(password, 10)
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(userId, {
+            ...data,
+            ...(updatedPassword && { password: updatedPassword })
+        });
+        res.send(updatedUser);
     } catch (error) {
         console.log(error)
         res.status(500).json({ message: "Failed to update user!" })
     }
 }
 
-export const deleteUser = (req, res) => {
-    console.log("deleteUser")
+export const deleteUser = async (req, res) => {
     try {
-
+        const result = await User.findByIdAndDelete(req.params.id);
+        res.status(204).json({ message: "User deleted successfully!", result: result })
     } catch (error) {
         console.log(error)
         res.status(500).json({ message: "Failed to delete user!" })
