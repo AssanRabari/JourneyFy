0464526ec1b88ba268ee4e3da66c549b9b3bd393
AssanRabari <assandesai@gmail.com>
2024-06-06 08:18:47 +0530

    ui: singlePost data integration

--- ui/src/pages/singlePage/SinglePage.jsx
+++ ui/src/pages/singlePage/SinglePage.jsx
@@ -7,7 +7,7 @@ import { singlePostData, userData } from "../../lib/dummyData";
 import { useLoaderData } from "react-router-dom";
 const SinglePage = () => {
   const post = useLoaderData();
-  console.log("post", post)
+  console.log("post", post);
   return (
     <div className="singlePage">
       <div className="details">
@@ -16,19 +16,19 @@ const SinglePage = () => {
           <div className="info">
             <div className="top">
               <div className="post">
-                <h1>{singlePostData.title}</h1>
+                <h1>{post.title}</h1>
                 <div className="address">
                   <img src="/pin.png" alt="" />
-                  <span>{singlePostData.address}</span>
+                  <span>{post.address}</span>
                 </div>
-                <div className="price">$ {singlePostData.price}</div>
+                <div className="price">$ {post.price}</div>
               </div>
               <div className="user">
                 <img src={userData.img} alt="" />
                 <span>{userData.name}</span>
               </div>
             </div>
-            <div className="bottom">{singlePostData.description}</div>
+            <div className="bottom">{post.description}</div>
           </div>
         </div>
       </div>
@@ -40,14 +40,14 @@ const SinglePage = () => {
               <img src="/utility.png" alt="" />
               <div className="featureText">
                 <span>Utilities</span>
-                <p>Renter is resposible</p>
+                <p>{post.postDetails[0]?.utilities}</p>
               </div>
             </div>
             <div className="feature">
               <img src="/pet.png" alt="" />
               <div className="featureText">
                 <span>Pet Policy</span>
-                <p>Pents Allowed</p>
+                <p>{post.postDetails[0]?.petPolicy}</p>
               </div>
             </div>
             <div className="feature">
@@ -62,15 +62,15 @@ const SinglePage = () => {
           <div className="sizes">
             <div className="size">
               <img src="/size.png" alt="" />
-              <span>80sq feet</span>
+              <span>{post.postDetails[0]?.size}sq feet</span>
             </div>
             <div className="size">
               <img src="/bed.png" alt="" />
-              <span>2 beds</span>
+              <span>{post?.bedroom} beds</span>
             </div>
             <div className="size">
               <img src="/bath.png" alt="" />
-              <span>1 bathroom</span>
+              <span>{post?.bathroom} bathroom</span>
             </div>
           </div>
           <p className="title">Nearby Places</p>
@@ -79,21 +79,21 @@ const SinglePage = () => {
               <img src="/school.png" alt="" />
               <div className="featureText">
                 <span>School</span>
-                <p>250m away</p>
+                <p>{post.postDetails[0]?.school}m away</p>
               </div>
             </div>
             <div className="feature">
               <img src="/bus.png" alt="" />
               <div className="featureText">
                 <span>Bus Stop</span>
-                <p>100m away</p>
+                <p>{post.postDetails[0]?.bus}m away</p>
               </div>
             </div>
             <div className="feature">
               <img src="/restaurant.png" alt="" />
               <div className="featureText">
                 <span>Restaurant</span>
-                <p>200m away</p>
+                <p>{post.postDetails[0]?.restaurant}m away</p>
               </div>
             </div>
           </div>
